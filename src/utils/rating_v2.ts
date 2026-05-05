c245006228a7ea1884669d45968c543bea48038d
tapu-gugu <415660840@qq.com>
2026-05-06 00:57:10 +0800

    Update rating_v2.ts

--- src/utils/rating_v2.ts
+++ src/utils/rating_v2.ts
@@ -172,7 +172,8 @@ export function calculateTaikoRating(
 
   // 计算爆发rating
   const burst_rt_base = calcDimensionRating(rating, songData.burst);
-  const burst_hs_factor = songData.handspeed > 0 ? MIN(rating / songData.handspeed, 1) : 1;
+  // const burst_hs_factor = songData.handspeed > 0 ? MIN(rating / songData.handspeed, 1) : 1;
+  const burst_hs_factor = songData.handspeed > 0 ? MIN(rating / songData.handspeed / MIN(rt_100 / songData.handspeed , 1) , 1) : 1;
   const burst_rt = burst_rt_base * burst_hs_factor;
 
   // 计算复合rating
@@ -180,9 +181,13 @@ export function calculateTaikoRating(
   const complex_penalty = (5000 / 9) * POWER(MAX(0.03 - input.bad_per, 0), 2) + 0.5;
   const complex_rt = complex_rt_base * complex_penalty;
 
+  // 计算部分满分rating用于计算节奏rating
+  const burst_rt_100 = calcDimensionRating(rt_100, songData.burst);
+  
   // 计算节奏rating
   const rhythm_rt_base = calcDimensionRating(rating, songData.rhythm);
-  const rhythm_burst_factor = songData.burst > 0 ? MIN(burst_rt / songData.burst, 1) : 1;
+  // const rhythm_burst_factor = songData.burst > 0 ? MIN(burst_rt / songData.burst, 1) : 1;
+  const rhythm_burst_factor = songData.burst > 0 ? MIN(burst_rt / songData.burst / MIN(burst_rt_100 / songData.burst , 1) , 1) : 1;
   const rhythm_rt = rhythm_rt_base *
     burst_hs_factor *
     rhythm_burst_factor;
